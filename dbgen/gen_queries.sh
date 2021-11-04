b5e1bf01f0433f3f8608f58af81a88a04bf5e22c
Xiaoying Wang <wangxiaoying0369@gmail.com>
2021-11-04 17:08:43 +0000

    update gen query script

--- dbgen/gen_queries.sh
+++ dbgen/gen_queries.sh
@@ -4,5 +4,5 @@ export DSS_QUERY=$CUR_DIR/queries
 echo $DSS_QUERY
 for ((i=1;i<=22;i++)); do
     # `-d`: default parameters
-    ./qgen -v -c -d -s 10 ${i} | sed 's/limit -1;//' | sed 's/day (3)/day/' > ./sf10/q${i}.sql
+    ./qgen -v -c -d -s 10 ${i} | sed '/^--/d' | sed '/^$/d' | sed '/^limit /d' | sed 's/;//' | sed 's/day (3)/day/' > ./sf10/q${i}.sql
 done
