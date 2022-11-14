16379cd383b0b7c9e7b8c2849733b46b70f4e641
Xiaoying Wang <xiaoying_wang@sfu.ca>
2022-11-14 22:10:20 +0000

    update

--- dbgen/duckdb.sql
+++ dbgen/duckdb.sql
@@ -1,9 +1,9 @@
 .read dss.ddl
-.import part.tbl part
-.import partsupp.tbl partsupp
-.import region.tbl region
-.import nation.tbl nation
-.import supplier.tbl supplier
-.import orders.tbl orders
-.import lineitem.tbl lineitem
-.import customer.tbl customer
+COPY lineitem FROM 'lineitem.tbl' (DELIMITER '|');
+COPY part FROM 'part.tbl' (DELIMITER '|');
+COPY customer FROM 'customer.tbl' (DELIMITER '|');
+COPY orders FROM 'orders.tbl' (DELIMITER '|');
+COPY supplier FROM 'supplier.tbl' (DELIMITER '|');
+COPY partsupp FROM 'partsupp.tbl' (DELIMITER '|');
+COPY nation FROM 'nation.tbl' (DELIMITER '|');
+COPY region FROM 'region.tbl' (DELIMITER '|');
