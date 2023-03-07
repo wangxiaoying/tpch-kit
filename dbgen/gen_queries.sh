7ee6ebd5aea0f0414741deeab87a772350968932
wangxiaoying <wangxiaoying0369@gmail.com>
2023-03-07 13:04:32 +0800

    add scripts for clickhouse and monetdb

--- dbgen/gen_queries.sh
+++ dbgen/gen_queries.sh
@@ -1,8 +1,8 @@
-mkdir sf10
+mkdir sf${SF}
 CUR_DIR=$(pwd)
 export DSS_QUERY=$CUR_DIR/queries
 echo $DSS_QUERY
 for ((i=1;i<=22;i++)); do
     # `-d`: default parameters
-    ./qgen -v -c -d -s 10 ${i} | sed '/^--/d' | sed '/^$/d' | sed '/^limit /d' | sed 's/;//' | sed 's/day (3)/day/' > ./sf10/q${i}.sql
+    ./qgen -v -c -d -s ${SF} ${i} | sed '/^--/d' | sed '/^$/d' | sed '/^limit /d' | sed 's/;//' | sed 's/day (3)/day/' > ./sf${SF}/q${i}.sql
 done
